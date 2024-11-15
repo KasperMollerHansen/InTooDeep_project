9548ab87ba903edb78850f1e074803e3e1fbf860
Kasper Møller Hansen <kasperhansen1998@hotmail.com>
2024-11-15 10:32:28 +0100

    Update

--- CNN.py
+++ CNN.py
@@ -1,6 +1,14 @@
 # %%
 # Load packages
+import os
+import pandas as pd
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+import torch
+import matplotlib.pyplot as plt
 import numpy as np
+from torch import nn
 
 #%%
 # Dataloader
