ec745f1a9bfd16e39c7a2715775345a0eb357bda
BjarkeHJ <bjarke_holm@hotmail.com>
2024-11-29 12:45:37 +0100

    large dataset

--- model_training/ResNet34.py
+++ model_training/ResNet34.py
@@ -52,7 +52,7 @@ def transform(image):
     return transform(image)
 
 angle_type = "both"
-batch_size = 16
+batch_size = 32
 images_num = 1
 base_angle_range = [0,360]
 model = nw.ResNet34
@@ -60,7 +60,7 @@ lr = 1e-2
 epochs = 40
 ############################################
 
-wind_dataset = wt.WindTurbineDataset(csv_file='rotations_w_images_60.csv', root_dir=root_dir+'/data/', 
+wind_dataset = wt.WindTurbineDataset(csv_file='rotations_w_images_long.csv', root_dir=root_dir+'/data/', 
                                      images_num=images_num, transform=transform, angle_type=angle_type, base_angle_range=base_angle_range)
 print(f"Dataset size: {len(wind_dataset)}")
 
